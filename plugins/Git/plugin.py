2d1b6bd05e949981e6b4ccb0db454bc85b63bf67
Mattia Rizzolo <mattia@mapreri.org>
2014-07-15 20:02:45 +0000

    plugins.git: use a more coloured commit_message as default

--- plugins/Git/plugin.py
+++ plugins/Git/plugin.py
@@ -113,7 +113,8 @@ class Repository(object):
         self.branch = 'origin/' + options.get('branch', 'master')
         self.channels = options.get('channels', options.get('channel')).split()
         self.commit_link = options.get('commit link', '')
-        self.commit_message = options.get('commit message', '[%s|%b|%a] %m')
+        self.commit_message = options.get('commit message', '''%(15)%![%!%(09)%a%(15)%!|%!%(4)%s%(15)%!|%!%(13)%b%(15)%!|%!%(2)%c%(15)%!]%! %m
+		View%!:%! %(10)%l''')
         self.commit_reply = options.get('commit reply', '')
         self.errors = []
         self.last_commit = None
