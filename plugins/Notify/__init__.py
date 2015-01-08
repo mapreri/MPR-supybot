580195efea79d37d42d39d865cebb2f30ad88730
Mattia Rizzolo <mattia@mapreri.org>
2015-01-08 21:37:16 +0000

    plugins.notify: port to python3

--- plugins/Notify/__init__.py
+++ plugins/Notify/__init__.py
@@ -34,6 +34,8 @@ Notify listenes to a port on its host for messages to post to channels.
 import supybot
 import supybot.world as world
 
+from imp import reload
+
 # Use this for the version of this plugin.  You may wish to put a CVS keyword
 # in here if you're keeping the plugin in CVS or some similar system.
 __version__ = '0.2.1'
@@ -50,14 +52,14 @@ __contributors__ = {supybot.Author('Tim Bielawa', 'tbielawa', 'timbielawa@gmail.
 # This is a url where the most recent plugin package can be downloaded.
 __url__ = 'https://fedorahosted.org/released/supybot-notify/'
 
-import config
-import plugin
+from . import config
+from . import plugin
 reload(plugin) # In case we're being reloaded.
 # Add more reloads here if you add third-party modules and want them to be
 # reloaded when this plugin is reloaded.  Don't forget to import them as well!
 
 if world.testing:
-    import test
+    from . import test
 
 Class = plugin.Class
 configure = config.configure
